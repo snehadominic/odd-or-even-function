091b319a347743eaa0f7f5b65b648db2f4e0bbf1
Sneha Dominic <sneha_dominic@outlook.com>
2020-11-20 08:59:41 +0000

    Updated config for asp in tf script

--- main.tf
+++ main.tf
@@ -30,10 +30,13 @@ resource "azurerm_app_service_plan" "asp" {
     name                = "fnapptest-asp"
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
+    kind                = "FunctionApp"
+    reserved            = true
+
     sku {
-    tier = "Standard"
-    size = "S1"
-  }
+        tier = "Dynamic"
+        size = "Y1"
+    }
 }
 
 # Create function app
